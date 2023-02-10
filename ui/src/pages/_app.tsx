43fce5ada241dc4ac8506e9cbd3f89631145066e
kalashshah <202051096@iiitvadodara.ac.in>
2023-02-10 22:44:49 +0530

    :sparkles: rainbowkit auth

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiConfig, configureChains, createClient } from "wagmi";
+import type { AppProps } from "next/app";
+import { polygonMumbai, goerli } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { chains, provider } = configureChains(
+    [goerli, polygonMumbai],
+    [publicProvider()]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "Pension Flow",
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} initialChain={goerli}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
