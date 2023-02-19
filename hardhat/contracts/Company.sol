248cc0b23fb1fe16ed6ec6cec0489e8058d17e07
Madhur Gupta <madhurgupta107@gmail.com>
2023-02-19 10:10:14 +0530

    Add company DAO variables and methods

--- hardhat/contracts/Company.sol
+++ hardhat/contracts/Company.sol
@@ -7,32 +7,67 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 error EmployeeAlreadyExists();
 error InsufficientFunds();
 
+struct Employee {
+    uint256 pensionStartDate;
+    uint256 pensionDuration;
+    uint256 monthyAmount;
+    uint256 employeeJoiningDate;
+    uint256 employeeLeavingDate;
+    uint256 minimumServiceRequired;
+}
+
+// creating the Request struct definition
+struct Proposal {
+    string description;
+    uint256 value;
+    string currency;
+    bool complete;
+    uint256 approvalCount; // number of yes
+    mapping(address => bool) votes; // mapping for the people who have voted
+}
+
 /**
  * @title Company
  * @dev This contract is used to store the details of a company and the pension details of its employees
  */
 contract Company is Initializable, OwnableUpgradeable {
+    // basic details of the company
     string public name;
     mapping(address => Employee) public employees;
     address payable[] public employeeAddresses;
 
-    uint256 internal lastDistributionTime;
-    uint public pensionCounter;
+    // DAO related details
+    uint256 noOfProposals;
+    mapping(uint256 => Proposal) proposals;
 
+    // Pension automtation related details
+    uint256 internal lastDistributionTime;
+    uint256 public pensionCounter;
+
+    // pension tranfer events
     event CompanyPensionTransferSucceeded(
         address indexed company,
         uint256 amount
     );
     event CompanyPensionTransferFailed(address indexed company);
 
-    struct Employee {
-        uint256 pensionStartDate;
-        uint256 pensionDuration;
-        uint256 monthyAmount;
-        uint256 employeeJoiningDate;
-        uint256 employeeLeavingDate;
-        uint256 minimumServiceRequired;
-        // bool employeeActive;
+    // UTILITY FUNCTIONS
+
+    /**
+     * @dev This function is used to check if an address is an employee or not
+     * @param addressToFind The address to be checked
+     */
+    function isAddressInEmployee(address addressToFind)
+        public
+        view
+        returns (bool)
+    {
+        for (uint256 i = 0; i < employeeAddresses.length; i++) {
+            if (employeeAddresses[i] == addressToFind) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -50,8 +85,60 @@ contract Company is Initializable, OwnableUpgradeable {
         name = _name;
 
         lastDistributionTime = block.timestamp;
-        pensionCounter=0;
+        pensionCounter = 0;
     }
+
+    // DAO RELATED FUNCTIONS
+
+    /**
+     * @dev This function is used to create a new proposal
+     * @param _description The description of the proposal
+     * @param _value The value of the proposal
+     * @param _currency The currency of the proposal
+     */
+    function createProposal(
+        string memory _description,
+        uint256 _value,
+        string memory _currency
+    ) public onlyOwner {
+        Proposal storage newRequest = proposals[noOfProposals++];
+        newRequest.description = _description;
+        newRequest.value = _value;
+        newRequest.currency = _currency;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+    }
+
+    /**
+     * @dev This function is used to approve a proposal
+     * @param proposalIndex The index of the proposal
+     */
+    function approveProposal(uint256 proposalIndex) public {
+        // checking if the caller of the function is an approver or not
+        require(isAddressInEmployee(msg.sender));
+
+        Proposal storage currProposal = proposals[proposalIndex];
+
+        // checking if the current approver hasnt voted before or not
+        require(!currProposal.votes[msg.sender]);
+
+        currProposal.votes[msg.sender] = true;
+        currProposal.approvalCount++;
+    }
+
+    function finalizeProposal(uint proposalIndex) public onlyOwner{
+        Proposal storage currProposal = proposals[proposalIndex];
+
+        require(currProposal.approvalCount > (employeeAddresses.length/2));
+
+        require(!currProposal.complete);       
+
+        // TODO - buy amount in that currency
+
+        currProposal.complete = true;
+    }
+
+    // PENSION RELATED FUNCTIONS
 
     /**
      * @dev This function is used to transfer funds to employess, its called automatically by performUpKeep
@@ -61,7 +148,7 @@ contract Company is Initializable, OwnableUpgradeable {
 
         uint256 interval = 2629056; // 1 month in seconds
         // uint256 interval = 60; // for dev
-        
+
         // Check if it is time to distribute funds
         if (block.timestamp >= lastDistributionTime + interval) {
             for (uint256 i = 0; i < employeeAddresses.length; i++) {
@@ -79,7 +166,6 @@ contract Company is Initializable, OwnableUpgradeable {
                 emit CompanyPensionTransferFailed(address(this));
                 revert InsufficientFunds();
             }
-
 
             // Transfer funds
             for (uint256 i = 0; i < employeeAddresses.length; i++) {
@@ -99,7 +185,7 @@ contract Company is Initializable, OwnableUpgradeable {
             );
 
             // Update the last distribution time
-            pensionCounter=pensionCounter+1;
+            pensionCounter = pensionCounter + 1;
             lastDistributionTime = block.timestamp;
         }
     }
